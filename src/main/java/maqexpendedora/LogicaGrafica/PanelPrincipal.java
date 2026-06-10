b90af1e033b50083b34d4af789a1fc2ed5385ebd
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-06-10 00:05:46 -0400

    Arreglar Package

--- src/main/java/maqexpendedora/LogicaGrafica/PanelPrincipal.java
+++ src/main/java/maqexpendedora/LogicaGrafica/PanelPrincipal.java
@@ -1,7 +1,7 @@
 package maqexpendedora.LogicaGrafica;
-import maqexpendedora.*;
-import maqexpendedora.Comprador;
-import maqexpendedora.Expendedor;
+
+import maqexpendedora.maqexpendedora.Comprador;
+import maqexpendedora.maqexpendedora.Expendedor;
 
 
 import javax.swing.*;
